fc32f679c24b2b8a603e0a4d4f9562aba62c8553
Artem Andreyuk <126854094+AndreyukDev@users.noreply.github.com>
2026-03-24 16:09:27 +0700

    feat(DBMQ): add a snakeToCamel in LegacyArrayAccess

--- src/DBMQ/LegacyArrayAccess.php
+++ src/DBMQ/LegacyArrayAccess.php
@@ -8,16 +8,22 @@ trait LegacyArrayAccess
 {
     public function offsetExists(mixed $offset): bool
     {
+        $offset = self::snakeToCamelStr($offset);
+
         return property_exists($this, $offset);
     }
 
     public function offsetGet(mixed $offset): mixed
     {
+        $offset = self::snakeToCamelStr($offset);
+
         return $this->$offset;
     }
 
     public function offsetSet(mixed $offset, mixed $value): void
     {
+        $offset = self::snakeToCamelStr($offset);
+
         if (property_exists($this, $offset)) {
             $this->$offset = $value;
         }
@@ -25,8 +31,19 @@ trait LegacyArrayAccess
 
     public function offsetUnset(mixed $offset): void
     {
+        $offset = self::snakeToCamelStr($offset);
+
         if (property_exists($this, $offset)) {
             $this->$offset = null;
         }
+    }
+
+    private static function snakeToCamelStr(mixed $snake): string
+    {
+        if (is_string($snake)) {
+            return lcfirst(str_replace('_', '', ucwords($snake, '_')));
+        }
+
+        return $snake;
     }
 }
